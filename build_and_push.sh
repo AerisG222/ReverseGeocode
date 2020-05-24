1e852431564568a4e5cf9f27c176eef4cac5f37c
Mike Morano <mmorano@mikeandwan.us>
2020-05-24 06:47:30 -0400

    fix how we get the image id

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+IID_FILE=image.iid
 TAG=$(date +%Y%m%d%H%M%S)
-IMAGE_ID=$(buildah bud -f Containerfile -t maw-reverse-geocode)
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IMAGE_ID}" docker://aerisg222/maw-reverse-geocode:"${TAG}"
+
+buildah bud -f Containerfile -t maw-reverse-geocode --iidfile "${IID_FILE}"
+
+IID=$(cat "${IID_FILE}")
+
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-reverse-geocode:"${TAG}"
