35b8891c07ebfd82c4f7903efd1f6cb372a91173
Mike Morano <mmorano@mikeandwan.us>
2020-06-09 07:40:26 -0400

    push out the latest tag to dockerhub

--- _build_and_push.sh
+++ _build_and_push.sh
@@ -7,3 +7,4 @@ buildah bud -f Containerfile -t maw-reverse-geocode --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-reverse-geocode:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-reverse-geocode:latest
