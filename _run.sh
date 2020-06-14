9a8950941fa66fe5193559160b9e6f8ef43d4147
Mike Morano <mmorano@mikeandwan.us>
2020-06-14 08:42:25 -0400

    remove cron hack

--- _run.sh
+++ _run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# poor mans cron
-while true; do
-    sleep 55m
+echo '*** executing reverse geocode ***'
 
-    echo '*** checking if we need to run reverse geocoder ***'
-
-    hour=$(date +"%H")
-
-    if [ "${hour}" = "01" ]; then
-        echo '*** executing reverse geocode ***'
-
-        /reverse-geocode/ReverseGeocode AUTO "${DB_CONN}" "${API_KEY}" /results
-    fi
-done
+/reverse-geocode/ReverseGeocode AUTO "${DB_CONN}" "${API_KEY}" /results
